bebc775a0dc1dc9e980a5fd591aa0c7092d8f444
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-06-08 11:58:31 +0530

    Add files via upload

---
+++ saral2.py
@@ -0,0 +1,7 @@
+def function_print_lines(name,post):
+    print("Mera naam",name,"hai.Main NavGurukul ka ",post)
+name=input("enter your name=")
+post=input("enter your post=")
+function_print_lines(name, post)
+
+# here we print name and post
