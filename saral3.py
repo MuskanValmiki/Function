c7a7e4c919b4283d25610bd093c13796f425cd69
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-06-08 11:58:51 +0530

    Add files via upload

---
+++ saral3.py
@@ -0,0 +1,13 @@
+def student_name(*name):
+    print(name)
+student_name("aditya","kajal","muskan","kashish","krishna","rakesh")
+
+# wethout list we gave more then one argument part1
+
+def isGreaterThen20(a,b=20):
+    print(a,b)
+isGreaterThen20(10)
+# part2 we gave default argument
+
+
+
