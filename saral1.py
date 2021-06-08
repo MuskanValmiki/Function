ed30f051fb9d95dc7b8eceb1bf441aa04167bde8
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-06-08 11:58:13 +0530

    Add files via upload

---
+++ saral1.py
@@ -0,0 +1,18 @@
+def ask_question():
+    print("ek baar")
+ask_question()
+ask_question()
+ask_question()
+ask_question()
+ask_question()
+
+# part1
+
+def ask_question():
+    i=1
+    while i<=100:
+        print(i,"ek baar")
+        i+=1
+ask_question()
+
+# part2
