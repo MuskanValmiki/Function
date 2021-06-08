f37da390ef4ccf17798df15ed6c212e135778363
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-06-08 11:57:50 +0530

    Add files via upload

---
+++ pre_difine.py
@@ -0,0 +1,25 @@
+# Q1 find max use pre-define function
+numbers = [3, 5, 7, 34, 2, 89, 2, 5] 
+print(max(numbers))
+
+# Q2 find sum use pre-define function
+numbers = [1, 2, 3, 4, 5] 
+print(sum(numbers))
+
+# Q3 find sort use pre-define function
+unorder_list = [6, 8, 4, 3, 9, 56, 0, 34, 7, 15] 
+unorder_list.sort()
+print(unorder_list)
+
+# Q4 find reverse use pre-define function
+reverse_list = ["Z", "A", "A", "B", "E", "M", "A", "R", "D"] 
+reverse_list.reverse()
+print(reverse_list)
+
+# Q5 find min use pre-define function
+list = [8, 6, 4, 8, 4, 50, 2, 7] 
+print(min(list))
+
+# Q6 find len use pre-define function
+a=[1,2,3,4,5,6]
+print(len(a)) 
