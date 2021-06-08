30f3bf1c7ca1e2747dc5601521335a0dc35783bb
MuskanValmiki <83203071+MuskanValmiki@users.noreply.github.com>
2021-06-08 12:00:57 +0530

    Add files via upload

---
+++ saral7.py
@@ -0,0 +1,9 @@
+def eligible_for_vote(age):
+    if age>=18:
+        print("eligible for vote")
+    else:
+        print("not eligible for vote")
+age=int(input("enter any age="))
+eligible_for_vote(age)
+
+# here we check it is eligible for vote or not
